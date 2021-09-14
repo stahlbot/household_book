67790353c5d28617a0cc2888a7414d923aae5640
Timon Stahlbock <stahlbot@uni-bremen.de>
2021-09-14 11:01:45 +0200

    Menubar and Drawer for Navigation

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Nav from "./Nav";
 // import HomePage from "./HomePage";
 
 export default class App extends Component {
@@ -9,9 +10,8 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="center">
-        {/*<HomePage />*/}
-        <h1>Hello</h1>
+      <div>
+        <Nav />
       </div>
     );
   }

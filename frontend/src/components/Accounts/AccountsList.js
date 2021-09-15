7176372bf6109ffd3668434e25fdf6558ee76aa6
Timon Stahlbock <stahlbot@uni-bremen.de>
2021-09-15 07:41:56 +0200

    Redisigned the New Account Button

--- frontend/src/components/Accounts/AccountsList.js
+++ frontend/src/components/Accounts/AccountsList.js
@@ -1,14 +1,27 @@
-import React, {useEffect, useState} from "react";
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import React from "react";
+import {
+    Button,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Toolbar,
+    Tooltip, Typography
+} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
+    table: {
+        minWidth: 650,
+    },
 });
 
-export default function AccountsList (props) {
+export default function AccountsList(props) {
     const classes = useStyles();
 
     const showAccount = (account) => {
@@ -18,26 +31,33 @@ export default function AccountsList (props) {
                 <TableCell>{account.name}</TableCell>
                 <TableCell>{account.created_at}</TableCell>
             </TableRow>
-            )
+        )
 
     }
 
 
+    return (
+        <Paper>
+            <Toolbar >
+                <Button aria-label="add" color={"primary"} variant={"outlined"} startIcon={<AddIcon/>}>
+                    New
+                </Button>
+            </Toolbar>
+            <TableContainer>
+                <Table className={classes.table} aria-label={"accounts"}>
 
-    return(
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label={"accounts"}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Created At</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.accounts.map(showAccount)}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Created At</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.accounts.map(showAccount)}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
     );
 }

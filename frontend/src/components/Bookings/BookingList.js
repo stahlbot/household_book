350ce3b30c5fdb6bbf108f1facbe145a97d5ef77
Timon Stahlbock <stahlbot@uni-bremen.de>
2021-09-17 12:34:29 +0200

    Display Bookings in a Table

--- frontend/src/components/Bookings/BookingList.js
+++ frontend/src/components/Bookings/BookingList.js
@@ -1,7 +1,27 @@
-import React, {useEffect} from "react";
-import {Card, CardContent, CardHeader, Divider} from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import {Card, CardContent, CardHeader, Divider, Table, TableBody, TableCell, TableRow} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 
 export default function BookingList() {
+
+    const [bookings, setBookings] = useState([{
+        "amount":0,
+        "offsetting_account":{
+            "id":0,
+            "name":"",
+            "created_at":"",
+            "account_type":"",
+            "get_account_type_display":""},
+        "date":"",
+        "account":{
+            "id":0,
+            "name":"Bank",
+            "created_at":"",
+            "account_type":"",
+            "get_account_type_display":""},
+        "text":""}]);
 
     useEffect(() => {
         const requestOptions = {
@@ -10,9 +30,24 @@ export default function BookingList() {
         fetch("api/bookings", requestOptions)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
+                console.log(data);
+                console.log(JSON.stringify(data))
+                setBookings(data);
             });
     }, []);
+
+    const showBooking = (booking) => {
+        console.log(booking.account.name)
+        return (
+            <TableRow>
+                <TableCell>{booking.amount}</TableCell>
+                <TableCell>{booking.offsetting_account.name}</TableCell>
+                <TableCell>{booking.date}</TableCell>
+                <TableCell>{booking.account.name}</TableCell>
+                <TableCell>{booking.text}</TableCell>
+            </TableRow>
+        );
+    }
 
     return(
         <React.Fragment>
@@ -20,7 +55,11 @@ export default function BookingList() {
                 <CardHeader title={"Bookings"}/>
                 <Divider/>
                 <CardContent>
-                    Tabelle
+                    <Table>
+                        <TableBody>
+                            {bookings.map(showBooking)}
+                        </TableBody>
+                    </Table>
                 </CardContent>
             </Card>
         </React.Fragment>
